f63976980bc744f0a64451635d49cdf48d2971f6
Nadeem Anvaritafti <nanvari@live.com>
2019-12-13 11:47:36 -0600

    added addProject function and router endpoint with validation

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -3,10 +3,10 @@ const db = require('../data/db-config');
 module.exports = {
     getProjects,
     getResources,
-    getTasks
-    // addProject,
-    // addResource,
-    // addTask
+    getTasks,
+    addProject,
+    addResource,
+    addTask
 }
 
 
@@ -22,4 +22,24 @@ function getTasks() {
     return db('tasks')
         .select('tasks.id', 'projects.name as project', 'projects.description as project_description', 'tasks.description as task', 'tasks.notes', 'tasks.completed')
         .join('projects', 'tasks.project_id', '=', 'projects.id');
+}
+
+function addProject(project) {
+    return db('projects')
+        .insert(project, 'id')
+        .then(ids => {
+            const [id] = ids;
+
+            return db('projects')
+                .where({ id })
+                .first();
+        })
+}
+
+function addResource() {
+
+}
+
+function addTask() {
+
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -23,4 +23,67 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/resources', (req, res) => {
+    Projects.getResources()
+    .then(resources => {
+        res.status(200).json(resources);
+    })
+    .catch(error => {
+        res.status(500).json({ errorMessage: 'Failed to get resources' })
+    })
+});
+
+
+router.get('/tasks', (req, res) => {
+    Projects.getTasks()
+    .then(tasks => {
+        for (let i=0; i<tasks.length; i++) {
+            if (tasks[i].completed === 0) {
+                tasks[i].completed = false;
+            } else if (tasks[i].completed === 1) {
+                tasks[i].completed = true;
+            }
+        }
+        res.status(200).json(tasks);
+    })
+    .catch(error => {
+        res.status(500).json({ errorMessage: 'Failed to get tasks' })
+    })
+});
+
+
+router.post('/', validateProject, (req, res) => {
+    const projectData = req.body;
+
+    Projects.addProject(projectData)
+    .then(project => {
+        if (project.completed === 0) {
+            project.completed = false;
+        } else if (project.completed === 1) {
+            project.completed = true;
+        }
+        res.status(201).json(project);
+    })
+    .catch(error => {
+        res.status(500).json({ errorMessage: 'Failed to add project' })
+    })
+});
+
+
+
+
+// ----------------------- CUSTOM MIDDLEWARE ------------------------ //
+function validateProject(req, res, next) {
+    const projectData = req.body;
+    if (!projectData) {
+        res.status(400).json({ error: 'missing project data' })
+    } else if (!projectData.name) {
+        res.status(400).json({ error: 'missing required project name' })
+    } else {
+        next();
+    }
+}
+
+
+
 module.exports = router;
